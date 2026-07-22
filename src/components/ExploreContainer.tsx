0377f6cbbb17eef92985f20cd48055637a995ef0
JosuePach3co <150095207+JosuePach3co@users.noreply.github.com>
2026-07-22 19:22:37 +0000

    feat: agregar alerta interactiva en el componente ExploreContainer

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,14 +1,25 @@
 import './ExploreContainer.css';
+import { IonAlert, IonButton } from '@ionic/react';
+import { useState } from 'react';
 
 interface ContainerProps {
   name: string;
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  const [isAlertOpen, setIsAlertOpen] = useState(false);
+
   return (
     <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonButton onClick={() => setIsAlertOpen(true)}>Click Me</IonButton>
+      <IonAlert
+        isOpen={isAlertOpen}
+        onDidDismiss={() => setIsAlertOpen(false)}
+        header="A Short Title Is Best"
+        subHeader="A Sub Header Is Optional"
+        message="A message should be a short, complete sentence."
+        buttons={['Action']}
+      ></IonAlert>
     </div>
   );
 };
